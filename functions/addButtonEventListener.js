1917b22a8e01652cf18beb0351ac636701bf5e3b
kephalosk <watchfromtheshore@gmail.com>
2024-08-15 19:08:14 +0200

    add logic for confirm and start button and add responsive design

--- functions/addButtonEventListener.js
+++ functions/addButtonEventListener.js
@@ -121,3 +121,159 @@ function resetValue(dessertContent) {
 
     updateCart(dessertButton, dessertButtonSelected);
 }
+
+export function addConfirmButtonEventListener() {
+    const cartBoxSelectedButton = document.querySelector('.cartBoxSelectedButton');
+
+    cartBoxSelectedButton.addEventListener('click', () => {
+        confirmOrder();
+    })
+
+    cartBoxSelectedButton.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter' || event.key === ' '){
+            confirmOrder();
+        }
+    })
+}
+
+function confirmOrder() {
+    updateConfirmationList();
+
+    const windowConfirmation = document.querySelector('.windowConfirmation');
+    const confirmation = document.querySelector('.confirmation');
+
+    windowConfirmation.classList.remove('hidden');
+    confirmation.classList.remove('hidden');
+}
+
+function updateConfirmationList() {
+    const confirmationListItems = document.querySelector('.confirmationListItems');
+    const items = confirmationListItems.querySelectorAll('section');
+    const cartBoxSelectedListItemContents = document.querySelectorAll('.cartBoxSelectedListItemContent');
+
+    items.forEach(item =>{
+        const confirmationListItemContentName = item.querySelector('.confirmationListItemContentName');
+
+        cartBoxSelectedListItemContents.forEach(cartBoxItem => {
+            const cartBoxSelectedListItemContentName = cartBoxItem.querySelector('.cartBoxSelectedListItemContentName');
+
+            if(confirmationListItemContentName.textContent === cartBoxSelectedListItemContentName.textContent){
+                transmitContent(item, cartBoxItem);
+            }
+        })
+    });
+
+    const confirmationListTotalPrice = document.querySelector('.confirmationListTotalPrice');
+    const cartBoxSelectedTotalPrice = document.querySelector('.cartBoxSelectedTotalPrice');
+    confirmationListTotalPrice.textContent = cartBoxSelectedTotalPrice.textContent;
+
+}
+
+function transmitContent(item, cartBoxItem) {
+    const confirmationListItemContentQuantityAmount = item.querySelector('.confirmationListItemContentQuantityAmount');
+    const cartBoxSelectedListItemContentQuantityAmount = cartBoxItem.querySelector('.cartBoxSelectedListItemContentQuantityAmount');
+    confirmationListItemContentQuantityAmount.textContent = cartBoxSelectedListItemContentQuantityAmount.textContent;
+
+    const confirmationListItemContentQuantitySum = item.querySelector('.confirmationListItemContentQuantitySum');
+    const cartBoxSelectedListItemContentQuantitySum = cartBoxItem.querySelector('.cartBoxSelectedListItemContentQuantitySum');
+    confirmationListItemContentQuantitySum.textContent = cartBoxSelectedListItemContentQuantitySum.textContent;
+
+    setVisibility(confirmationListItemContentQuantityAmount, item);
+}
+
+function setVisibility(confirmationListItemContentQuantityAmount, item) {
+    const amountRaw = confirmationListItemContentQuantityAmount.textContent;
+    const amountClean = amountRaw.replace('x','');
+    const amount = parseInt(amountClean);
+
+    if (amount === 0) {
+        item.classList.add('hidden');
+    } else {
+        item.classList.remove('hidden');
+    }
+}
+
+export function addStartButtonEventListener() {
+    const confirmationButton = document.querySelector('.confirmationButton');
+
+    confirmationButton.addEventListener('click', () => {
+        startOrder();
+    })
+
+    confirmationButton.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter' || event.key === ' '){
+            startOrder();
+        }
+    })
+}
+
+function startOrder() {
+    const windowConfirmation = document.querySelector('.windowConfirmation');
+    const confirmation = document.querySelector('.confirmation');
+
+    windowConfirmation.classList.add('hidden');
+    confirmation.classList.add('hidden');
+
+    resetValues();
+}
+
+function resetValues() {
+    resetDessertButtons();
+    resetCartBoxList();
+}
+
+function resetDessertButtons() {
+    const dessertsGridItems = document.querySelectorAll('.dessertsGridItemComponent');
+
+    dessertsGridItems.forEach(item => {
+        const dessertButtonLabel = item.querySelector('.dessertButtonSelectedLabel');
+        dessertButtonLabel.textContent = '0';
+
+        const dessertButton = item.querySelector('.dessertButtonComponent');
+        const dessertButtonSelected = item.querySelector('.dessertButtonSelectedComponent');
+        dessertButton.classList.remove('hidden');
+        dessertButtonSelected.classList.add('hidden');
+    });
+}
+
+function resetCartBoxList() {
+    const cartBoxSelectedListItem = document.querySelectorAll('.cartBoxSelectedListItem');
+
+    cartBoxSelectedListItem.forEach(item => {
+        resetItemAmount(item);
+        resetItemSum(item);
+
+        const parentSection = item.parentElement;
+        if(!parentSection.classList.contains('hidden')) {
+            parentSection.classList.add('hidden');
+        }
+    });
+
+    resetTotalAmount();
+    resetTotalSum();
+
+    const cartBoxEmpty = document.querySelector('.cartBoxEmpty')
+    const cartBoxSelected = document.querySelector('.cartBoxSelected');
+    cartBoxEmpty.classList.remove('hidden');
+    cartBoxSelected.classList.add('hidden');
+}
+
+function resetItemAmount(item) {
+    const cartBoxSelectedListItemContentQuantityAmount = item.querySelector('.cartBoxSelectedListItemContentQuantityAmount');
+    cartBoxSelectedListItemContentQuantityAmount.textContent = '0x';
+}
+
+function resetItemSum(item) {
+    const cartBoxSelectedListItemContentQuantitySum = item.querySelector('.cartBoxSelectedListItemContentQuantitySum');
+    cartBoxSelectedListItemContentQuantitySum.textContent = '$0.00';
+}
+
+function resetTotalAmount() {
+    const itemsAmountTotal = document.querySelector('.itemsAmountTotal');
+    itemsAmountTotal.textContent = '0';
+}
+
+function resetTotalSum() {
+    const cartBoxSelectedTotalPrice = document.querySelector('.cartBoxSelectedTotalPrice');
+    cartBoxSelectedTotalPrice.textContent = '$0.00';
+}
--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 import { loadComponents } from "./functions/loadComponents.js";
-import { addButtonEventListener, addRemoveButtonEventListener } from "./functions/addButtonEventListener.js";
+import {
+    addButtonEventListener,
+    addConfirmButtonEventListener,
+    addRemoveButtonEventListener,
+    addStartButtonEventListener
+} from "./functions/addButtonEventListener.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
     await loadComponents();
     addButtonEventListener();
     addRemoveButtonEventListener();
+    addConfirmButtonEventListener();
+    addStartButtonEventListener();
 });
